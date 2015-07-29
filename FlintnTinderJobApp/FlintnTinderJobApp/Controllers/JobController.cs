42c0bbdc7d43c494dd0fb4fa886183652f2abe13
GustoTau <gontse@outlook.com>
2015-07-29 15:47:41 +0200

    Job app assessment
    Job app assessment

---
+++ FlintnTinderJobApp/FlintnTinderJobApp/Controllers/JobController.cs
@@ -0,0 +1,50 @@
+﻿using FlintnTinderJobApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlintnTinderJobApp.Controllers
+{
+    public class JobController : Controller
+    {
+        //
+        // GET: /Job/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetAllJobs(string search)
+        {
+            JobObject model = new JobObject();
+            Jobs newjobsList = new Jobs();
+
+            using (StreamReader rd = new StreamReader(@"c:\users\gontse.tauyane\documents\visual studio 2012\Projects\FlintnTinderJobApp\FlintnTinderJobApp\Jobs.json"))
+            {
+                string json = rd.ReadToEnd();
+                model = Newtonsoft.Json.JsonConvert.DeserializeObject<JobObject>(json);
+            }
+            
+            //Attempting to do the search
+            if (!String.IsNullOrEmpty(search))
+            {
+                for (int i = 0; i < model.jobs.Count; i++)
+                {
+                    if (model.jobs[i].Client.IndexOf(search, StringComparison.OrdinalIgnoreCase) != -1)
+                    {
+                        newjobsList = model.jobs[i];
+                    }
+                }
+            }
+            else
+            {
+                return View(model);
+            }
+            return View(newjobsList);
+        }
+    }
+}
---
+++ FlintnTinderJobApp/FlintnTinderJobApp/Models/Jobs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FlintnTinderJobApp.Models
+{
+    public class Jobs
+    {
+        public string Client { get; set; }
+        public int JobNumber { get; set; }
+        public string JobName { get; set; }
+        public DateTime Due { get; set; }
+        public string Status { get; set; }
+    }
+    public class JobObject
+    {
+        public List<Jobs> jobs { get; set; }
+        
+    }
+}
